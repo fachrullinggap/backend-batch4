884c3fa4f2fcdb75c3f848d3ef9fe909cf153b0a
fachrullinggap <fachrullinggap@gmail.com>
2025-08-06 21:39:44 +0700

    Connect to database PostgreSQL

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 
 //import router
 const router = require('./routes/router');
 const userRouter = require('./routes/userRouter');
+const todoCRouter = require('./routes/todoRouter')
 
 //import custom middleware
-const {logger} = require('./middlewares/logger')
+const {logger} = require('./middlewares/logger');
 
 const app = express();
 
+//use middlewares
 app.use(logger);
+app.use(compression())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,6 +23,7 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use('/', router);
 app.use('/api/user', userRouter);
+app.use('/api/todo', todoCRouter)
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log('Server Running');
